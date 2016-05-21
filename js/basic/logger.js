18dbc0fd9ede66d1469eb4e714f50f94845de5bc
James Putnam <jonathan.putnam86@gmail.com>
2016-05-21 17:13:10 -0600

    logger and LongestWord

--- js/basic/logger.js
+++ js/basic/logger.js
@@ -2,3 +2,12 @@
 // method one: logs out msg with word INFO: prefixed
 // method two: logs err to console with ERROR: prefixed
 
+var logger = {
+  msg: function(){
+    alert("INFO: prefixed")
+  },
+  err: function() {
+    alert("ERROR: prefixed")
+  }
+
+};
--- js/basic/longest-word-in-string.js
+++ js/basic/longest-word-in-string.js
@@ -1,3 +1,17 @@
 // Write a function that returns the longest word(s)
 // from a sentence.  The function should not return
 // any duplicate words (case-insensitive).
+function longestWord(str){
+	var newStr = str.split(' ');
+	var longest = 0;
+	var word = '';
+	for (var i = 0; i < newStr.length; i++){
+		if(newStr[i].length > longest){
+			longest = newStr.length;
+			word = newStr[i];
+		}
+	}
+	return word;
+}
+
+longestWord("I think you should consider the factoidss");
