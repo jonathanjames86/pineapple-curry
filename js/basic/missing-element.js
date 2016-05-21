5e234d437cbbaea008aabfbd0653ab8205aeee11
James Putnam <jonathan.putnam86@gmail.com>
2016-05-21 17:32:49 -0600

    missing-elements done

--- js/basic/missing-element.js
+++ js/basic/missing-element.js
@@ -3,3 +3,15 @@
 // Given these two arrays, find which element is missing in the second array.
 
 // There are most concise ways to write this; this solution avoids nested loops.
+function missing(arr){
+	var missNum = [];
+	newArray = arr.sort();
+	for(var i = 0; i < newArray.length - 1; i++){
+		if(newArray[i + 1] - newArray[i] !== 1){
+			missNum.push(newArray[i] + 1);
+
+		}
+	}
+	return missNum;
+}
+missing();
