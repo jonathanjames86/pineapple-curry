1e9435bcb3959afa42793c4f7c47d0a99aba7ddc
James Putnam <jonathan.putnam86@gmail.com>
2016-05-21 16:35:01 -0600

    NonRepeated Chars done

--- js/basic/first-non-repeated-character-in-a-string.js
+++ js/basic/first-non-repeated-character-in-a-string.js
@@ -3,3 +3,12 @@
 // For example:
 // firstNonRepeatedCharacter(‘ABA’); // => ‘B’
 // firstNonRepeatedCharacter(‘AABCABD’); // => ‘C’
+function firstNonRepeatedCharacter(str){
+  for(var i = 0; i < str.length; i++){
+    var chr = string.charAt(i);
+    if(str.indexOf(chr) === i && str.index(c, i + 1) === -1) {
+      return c;
+    }
+  }
+  return null;
+}
