1729f9f04121401f5819f6e67c287a365a42c185
James Putnam <jonathan.putnam86@gmail.com>
2016-05-21 17:01:10 -0600

    basic-bro halfway

--- js/basic/is-boolean.js
+++ js/basic/is-boolean.js
@@ -1,1 +1,9 @@
-// write a function that just checks if what you pass it is a boolean
+// write a function that just checks if what you pass in is a boolean
+function isB(input){
+	if(typeof(input) === "boolean"){
+		return true;
+	}
+	return false;
+}
+
+isB(true);
--- js/basic/length-of-average-word-in-a-string.js
+++ js/basic/length-of-average-word-in-a-string.js
@@ -1,1 +1,13 @@
 // find the average length of each word in a string
+function average(str){
+	var newStr = str.split(' ');
+	var total = 0;
+	for(var i = 0; i < newStr.length; i++){
+		total = total + newStr[i].length;
+	}
+	var avg = total / newStr.length;
+	return avg;
+
+}
+
+average();
