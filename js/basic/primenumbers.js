3b0129a9a5cfe00299822a1f4909bd64d4f4bb14
James Putnam <jonathan.putnam86@gmail.com>
2016-05-21 18:28:14 -0600

    primenumbers

--- js/basic/primenumbers.js
+++ js/basic/primenumbers.js
@@ -1,1 +1,9 @@
 // Write a function that accepts a number and returns a boolean based on whether it's a prime number.
+function prime(num){
+  for(var i = 2; i < num; i++){
+    if(num % i === 0){
+      return false;
+    }
+  }
+  return true;
+}
